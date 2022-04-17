299755a44239c21cbb460444ef1547671940f573
SergantChe <sergei.alex.kozlov@gmail.com>
2022-04-17 15:04:00 +0300

    add new trpc query

--- pages/api/trpc/[trpc].ts
+++ pages/api/trpc/[trpc].ts
@@ -2,8 +2,13 @@ import * as trpc from '@trpc/server';
 import * as trpcNext from '@trpc/server/adapters/next';
 import { z } from 'zod';
 
+const nearAPI = require("near-api-js");
+const getConfig = require("../../../config/near");
+const nearConfig = getConfig('testnet');
+
 export const appRouter = trpc
   .router()
+  // Hello world endpoint
   .query('hello', {
     input: z
       .object({
@@ -13,6 +18,20 @@ export const appRouter = trpc
     resolve({ input }) {      
       return {
         greeting: `hello ${input?.text ?? 'world'}`,
+      };
+    },
+  })
+  // Second endpoint
+  .query('upload-evidence', {
+    input: z
+      .object({
+        text: z.string().nullish(),
+      })
+      .nullish(),
+    resolve({ input }) {
+      console.log(nearConfig);
+      return {
+        greeting: `hello ${input?.text ?? 'world 2'}`,
       };
     },
   });
