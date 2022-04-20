14fa3f166c3d4ed8adb3f65aca13dbdaccd0db12
SergantChe <sergei.alex.kozlov@gmail.com>
2022-04-20 21:01:16 +0300

    code cleaning

--- pages/api/upload-evidence.ts
+++ pages/api/upload-evidence.ts
@@ -1,37 +1,27 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
-import type { AppRouter } from './trpc/[trpc]';
-import { createTRPCClient } from '@trpc/client';
 import { getConnectedContract } from '../../utils/contract';
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
     try {
         const { query } = req;
         const { data } = query;
-        console.log('Query: ', data );
-        const parsedData = JSON.parse(data.slice(1, -1));
-        console.log(parsedData);
-        const { hash, name, time, location } = parsedData;
+        let parsedData = JSON.parse(data.slice(1, -1));
+        const { hash } = parsedData;
         parsedData['uploadThrough'] = 'server';
         delete parsedData.hash;
-        console.log(parsedData);
+        parsedData = JSON.stringify(parsedData);
+
+        // Create contract instance to use its method 
         const contract = await getConnectedContract();
         await contract.upload_evidence(
         {
           evidence: {
             media_hash: hash,
-            metadata: JSON.stringify({
-              author: 'Serg',
-              uploadThrough: 'server',
-            }),
+            metadata: parsedData,
           },
         },
         "300000000000000" // attached GAS (optional)
       );
-      let response = await contract.get_evidences({
-        from_index: 10,
-        limit: 10
-      });
-      console.log(response);
       res.status(200).json({ result: true });
     } catch (err) {
         console.log(err);
