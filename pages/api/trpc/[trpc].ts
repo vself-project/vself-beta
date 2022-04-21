14fe3c72b4005e6ae79b2fa08f6e8cbfc03473f7
Vasily Kharlamov <caesai@yandex.ru>
2022-04-21 14:26:08 +0300

    TRPC Fixes

--- pages/api/trpc/[trpc].ts
+++ pages/api/trpc/[trpc].ts
@@ -2,16 +2,17 @@ import * as trpc from '@trpc/server';
 import * as trpcNext from '@trpc/server/adapters/next';
 import { z } from 'zod';
 import * as nearAPI from 'near-api-js';
+import { readFileSync } from 'fs';
+
 import getConfig from '../../../config/near';
 import { Endpoints } from '../../../constants/endpoints';
 
 const contractName = Endpoints.TESTNET_POW_CONTRACT_NAME;
 const accountName = 'pow_v1.sergantche.testnet';
-const contractMethods = 
-{
+const contractMethods = {
   viewMethods: ['get_evidences', 'version'],
   changeMethods: ['upload_evidence'],
-}
+};
 
 const {
   keyStores: { InMemoryKeyStore },
@@ -25,36 +26,24 @@ const {
 } = nearAPI;
 
 // Read wallet credentials
-const fs = require("fs");
-const credentials = JSON.parse(
-  fs.readFileSync(
-    `./creds/${accountName}.json`    
-  )
-);
+const creds = readFileSync(`./creds/${accountName}.json`);
+const credentials = JSON.parse(JSON.stringify(creds));
 
 // Create keyStore object
 const keyStore = new InMemoryKeyStore();
 const { nodeUrl, networkId } = getConfig('testnet');
-keyStore.setKey(
-  networkId,
-  accountName,
-  KeyPair.fromString(credentials.private_key)
-);
+keyStore.setKey(networkId, accountName, KeyPair.fromString(credentials.private_key));
 
 // Add access key into calling contract account
 const { connection } = new Near({
   networkId,
   nodeUrl,
   deps: { keyStore },
+  headers: {},
 });
 const contractAccount = new Account(connection, accountName);
-contractAccount.addAccessKey = (publicKey) =>
-  contractAccount.addKey(
-    publicKey,
-    contractName,
-    contractMethods.changeMethods,
-    parseNearAmount("0.1")
-  );
+// contractAccount.addAccessKey = (publicKey) =>
+//   contractAccount.addKey(publicKey, contractName, contractMethods.changeMethods, parseNearAmount('0.1'));
 
 // Create callable contract instance
 const contract = new Contract(contractAccount, contractName, contractMethods);
@@ -68,7 +57,7 @@ export const appRouter = trpc
         text: z.string().nullish(),
       })
       .nullish(),
-    resolve({ input }) {      
+    resolve({ input }) {
       return {
         greeting: `hello ${input?.text ?? 'world'}`,
       };
--- utils/contract.ts
+++ utils/contract.ts
@@ -1,59 +1,48 @@
-import * as nearAPI from 'near-api-js';
+// import * as nearAPI from 'near-api-js';
+import { Near, Account, keyStores, Contract, KeyPair } from 'near-api-js';
 import getConfig from '../config/near';
 import { Endpoints } from '../constants/endpoints';
+import fs from 'fs';
 
 // Constants
 const contractName = Endpoints.TESTNET_POW_CONTRACT_NAME;
 const accountName = 'pow_v1.sergantche.testnet';
-const contractMethods = 
-{
+const contractMethods = {
   viewMethods: ['get_evidences', 'version'],
   changeMethods: ['upload_evidence'],
-}
+};
 
 export const getConnectedContract = async () => {
-  const {
-    keyStores: { InMemoryKeyStore },
-    Near,
-    Account,
-    Contract,
-    KeyPair,
-    utils: {
-      format: { parseNearAmount },
-    },
-  } = nearAPI;
+  const { InMemoryKeyStore } = keyStores;
+  // const {
+  //   keyStores: { InMemoryKeyStore },
+  //   Near,
+  //   Account,
+  //   Contract,
+  //   KeyPair,
+  //   utils: {
+  //     format: { parseNearAmount },
+  //   },
+  // } = nearAPI;
 
   // Read wallet credentials
-  const fs = require("fs");
-  const credentials = JSON.parse(
-    fs.readFileSync(
-      `./creds/${accountName}.json`    
-    )
-  );
+  const credentials = JSON.parse(JSON.stringify(fs.readFileSync(`./creds/${accountName}.json`)));
 
   // Create keyStore object
   const keyStore = new InMemoryKeyStore();
   const { nodeUrl, networkId } = getConfig('testnet');
-  keyStore.setKey(
-    networkId,
-    accountName,
-    KeyPair.fromString(credentials.private_key)
-  );
+  keyStore.setKey(networkId, accountName, KeyPair.fromString(credentials.private_key));
 
   // Add access key into calling contract account
   const { connection } = new Near({
     networkId,
     nodeUrl,
     deps: { keyStore },
+    headers: {},
   });
   const contractAccount = new Account(connection, accountName);
-  contractAccount.addAccessKey = (publicKey) =>
-    contractAccount.addKey(
-      publicKey,
-      contractName,
-      contractMethods.changeMethods,
-      parseNearAmount("0.1")
-    );
+  // contractAccount.addAccessKey = (publicKey) =>
+  //   contractAccount.addKey(publicKey, contractName, contractMethods.changeMethods, parseNearAmount('0.1'));
 
   // Create callable contract instance
   const contract = new Contract(contractAccount, contractName, contractMethods);
