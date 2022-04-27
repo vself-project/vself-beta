c5761e6f8a905456b8460e9cd4ddeb4240c98043
SergantChe <sergei.alex.kozlov@gmail.com>
2022-04-27 23:43:55 +0300

    Finish image block

--- features/dashboardTable/index.tsx
+++ features/dashboardTable/index.tsx
@@ -1,12 +1,12 @@
 /* eslint-disable jsx-a11y/alt-text */
 /* eslint-disable @next/next/no-img-element */
 import React, { useState } from 'react';
-import { renderFirebaseImage } from '../../utils/firebase';
 
 // Components
 import Spinner from '../../components/spinner';
 import HashDoxLogo from '../../components/icons/HashDoxLogo';
 import { Evidence } from '../../models/Evidence';
+import URLImageComponent from '../../components/urlImage';
 
 interface ImageLocation {
   latitude: number;
@@ -20,26 +20,30 @@ interface DashboardTableProps {
 
 // Constants
 const TRX_HASH_EXAMPLE = '2mtMSbfb26ojrn8ZPRwodExQDWkM4qw2wGDhPQrnSATj';
+
+// Return human readable string with date and time
 const getDateFromTimestamp = (timestamp: any) => {
   const date = new Date(timestamp);
   return date.getDate() + '/' + (date.getMonth() + 1) + '/' + date.getFullYear() + " " + date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds();
 }
+
+// Return url of image
 const getImageSource = (evidence: Evidence) => {
   const { metadata, media_hash } = evidence;
   const metadataObject = JSON.parse(metadata);
   if (metadataObject.hasOwnProperty('uploadThrough') && metadataObject.uploadThrough == 'server') {
-    //return "https://mdbootstrap.com/img/new/standard/nature/184.jpg";
-    const url = "http://82.148.29.178/images/" + media_hash + ".png";
-    return url;
+    // Images uploaded by mobile app
+    return "http://82.148.29.178/images/" + media_hash + ".png";
   }
+  // Get url of image storaged in firebase (uploaded through firebase) 
   return "https://firebasestorage.googleapis.com/v0/b/vself-dev.appspot.com/o/images%2F" + media_hash + ".png?alt=media";
-  // Firebase image data
-  // "https://firebasestorage.googleapis.com/v0/b/vself-dev.appspot.com/o/images%2F" + media_hash + ".png"
 }
 
-// TO DO check that case
+// TO DO remove 'from' prop
 const DashboardTable: React.FC<DashboardTableProps> = ({ evidences, from }) => {
   const [activeEvidenceIndex, setActiveEvidenceIndex] = useState(0);
+
+  // TO DO check that case
   if (!evidences.length) {
     return (
       <div className="grid place-items-center h-screen">
@@ -51,25 +55,8 @@ const DashboardTable: React.FC<DashboardTableProps> = ({ evidences, from }) => {
       </div>
     );
   }
-  console.log({ evidences });
 
-  const getImageCard = () => {
-    return (
-      <div className="rounded-lg shadow-lg bg-white max-w-sm h-full">
-        <a href="#!">
-          <img className="rounded-t-lg" src="https://mdbootstrap.com/img/new/standard/nature/184.jpg" alt="The image isn't available."/>
-        </a>
-        <div className="p-6">
-          <h5 className="text-gray-900 text-xl font-medium mb-2">Data</h5>
-          <p className="text-gray-700 text-base mb-4">
-            Some quick example text to build on the card title and make up the bulk of the card's
-            content.
-          </p>
-        </div>
-      </div>
-    )
-  }
-
+  // Return block with transactions data
   const getTransactionsBlock = () => {
     return (
       <div className="flex flex-1 flex-col">
@@ -100,6 +87,16 @@ const DashboardTable: React.FC<DashboardTableProps> = ({ evidences, from }) => {
     )
   }
 
+  // Return block with corresponding image
+  const getImageBlock = () => {
+    const image_url = getImageSource(evidences[activeEvidenceIndex]);
+    return (
+      <div className="p-2 flex flex-1 justify-center align-center sm:justify-start"> 
+        <URLImageComponent url={image_url} className="h-80 rounded max-w-sm"/>
+      </div>
+    )
+  }
+
   const getMapBlock = () => {
     return (
       <div className="h-64 bg-stone-500">Map</div>
@@ -110,33 +107,16 @@ const DashboardTable: React.FC<DashboardTableProps> = ({ evidences, from }) => {
     <div style={{ display: 'flex', flexDirection: 'column', width: '100%', height: '100vh' }}>
       <div className="flex flex-1 flex-col sm:flex-row">
         {getTransactionsBlock()}
-        <div style={{ display: 'flex', flex: 1, backgroundColor: 'brown' }}>
-          <img className="" src={getImageSource(evidences[activeEvidenceIndex])} alt="The image isn't available."/>
-        </div>
+        {getImageBlock()}
       </div>
       <div className="flex flex-1 flex-col sm:flex-row">
         <div style={{ display: 'flex:', flex: 1, backgroundColor: 'yellow' }}>
-          3
+          {getTransactionsBlock()}
         </div>
         <div style={{ display: 'flex', flex: 1, backgroundColor: 'green' }}>
-          4
+          {getTransactionsBlock()}
         </div>
       </div>
-      {/* <div className="flex justify-center	bg-stone-100">
-        <div className="flex flex-row columns-2 bg-stone-400">
-          <div className="flex flex-col basis-1/2 bg-orange-400 max-w-2xl">
-            <div className="block rounded-lg shadow-lg bg-white p-4 pr-10 mr-2 mb-2">
-              {getDataTable()}
-            </div>
-            <div className="block rounded-lg shadow-lg bg-white p-4 mr-2">
-             {getMapBlock()}
-            </div>
-          </div>
-          <div className="basis-1/2">
-          {getImageCard()}
-          </div>
-        </div>
-      </div> */}
     </div>
   );
 };
